eed6ca86d98aca70fd5260777a431b8011799132
saiful-100 <sahidulx47@gmail.com>
2026-03-08 22:48:51 +0600

    added js in my login page

---
+++ script/login.js
@@ -0,0 +1,12 @@
+document.getElementById('btn-sign-in').addEventListener("click",(e)=>{
+    e.preventDefault();
+    const url = document.getElementById("Username-input").value;
+    // console.log(url);
+    const password = document.getElementById("password-input").value;
+    // console.log(password);
+    if(url === "admin" && password === "admin123"){
+        window.location.href = "../index.html";
+    }else{
+        alert("Invalid Credentials");
+    }
+})
